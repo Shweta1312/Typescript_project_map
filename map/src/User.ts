70d23495082781a541a829d965801da9b5b4e0eb
Shweta1312 <shawant11@gmail.com>
2020-10-22 16:55:03 +0530

    Using faker & its type definition file to generate random info

---
+++ map/src/User.ts
@@ -0,0 +1,17 @@
+import faker from 'faker';
+
+export class User {     //Export this class to index.ts
+  name: string;
+  location: {       //declared the object
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {       //We have to initialize here.....so can't use this.location.lat = 20
+      lat: parseFloat(faker.address.latitude()),  
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
+}
