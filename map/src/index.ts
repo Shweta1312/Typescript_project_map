31c215bdd5597ef4d36bb16541a868c0537c1e30
Shweta1312 <shawant11@gmail.com>
2020-10-22 18:20:08 +0530

    Installed googlemaps & its type definition file and displayed map on browser

--- map/src/index.ts
+++ map/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hii there!');
+// import { User } from './User';
+// import { Company } from './Company';
+
+
+new google.maps.Map(document.getElementById('map'),{
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
+
